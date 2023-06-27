404ad0389ae324f1c6a783f4348ff0ce82b8995a
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-06-28 00:56:02 +0300

    Add google font

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.css';
+import './global.css';
 import App from './App';
 import store from './redux/store';
 
