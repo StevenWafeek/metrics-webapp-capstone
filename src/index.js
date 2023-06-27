ceec5f0c63dd5cb6d7c98d81879c63c9e381dba8
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-06-28 01:47:28 +0300

    Fix index file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import './index.css';
 import './global.css';
 import App from './App';
 import store from './redux/store';
 
-ReactDOM.render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
