1fb5d2e8734f7b695603fcd08159ad20cf926a32
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-06-27 08:24:29 +0300

    Update app .js file

--- src/App.js
+++ src/App.js
@@ -1,44 +1,12 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-} from 'react-router-dom';
-import StockList from './component/stocklist';
-import StockDetails from './component/stockDetails';
-import './App.css';
+
+import Cards from './component/cards';
 
 function App() {
   return (
-    <Router>
-      <div className="app">
-        <nav>
-          <ul className="header-ul">
-            <li>
-              <h1 className="headerName">StockStore</h1>
-            </li>
-            <li>
-              <Link to="/">STOCKS</Link>
-            </li>
-            <li>
-              <Link to="/details">details</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route
-            path="/"
-            element={(
-              <div>
-                <StockList />
-              </div>
-          )}
-          />
-          <Route path="/details" element={<StockDetails />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <Cards />
+    </div>
   );
 }
 
