80c3b7b56e636fd2aa8b4a01df2ead941477475c
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-06-28 11:25:07 +0300

    Test the search bar

---
+++ src/component/Filter.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import Filter from './Filter';
+
+describe('Filter', () => {
+  it('renders the filter component', () => {
+    const searchTerm = 'test';
+    const onSearchMock = jest.fn();
+
+    render(<Filter searchTerm={searchTerm} onSearch={onSearchMock} />);
+
+    const inputElement = screen.getByPlaceholderText('Search...');
+    expect(inputElement).toBeInTheDocument();
+    expect(inputElement.value).toBe(searchTerm);
+  });
+
+  it('calls onSearch function when input value changes', () => {
+    const searchTerm = 'test';
+    const onSearchMock = jest.fn();
+
+    render(<Filter searchTerm={searchTerm} onSearch={onSearchMock} />);
+
+    const inputElement = screen.getByPlaceholderText('Search...');
+    const newSearchTerm = 'new test';
+    fireEvent.change(inputElement, { target: { value: newSearchTerm } });
+
+    expect(onSearchMock).toHaveBeenCalledTimes(1);
+    expect(onSearchMock).toHaveBeenCalledWith(newSearchTerm);
+  });
+});
