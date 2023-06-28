36e9a60ddaa9350fff024f837c29748c1d90b6e2
StevenWafeek <117098454+StevenWafeek@users.noreply.github.com>
2023-06-28 11:27:06 +0300

    Add test filter to test component

--- src/test/Filter.test.js
+++ src/test/Filter.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import Filter from './Filter';
+import Filter from '../component/Filter';
 
 describe('Filter', () => {
   it('renders the filter component', () => {
